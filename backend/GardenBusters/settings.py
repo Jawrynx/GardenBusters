97c882a62167694c293a47608f455f1c336d3e08
Jawrynx <jawrynx@gmail.com>
2025-02-23 14:21:22 +0000

    Fix for deployment

--- backend/GardenBusters/settings.py
+++ backend/GardenBusters/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'landscaping_api',
+    'GardenBusters.landscaping_api',
     'corsheaders',
 ]
 
